84f6b40d06af5e3fd124ae94f5816d1f926aa78a
Aaron Agosto <128488135+AaronMAgosto@users.noreply.github.com>
2023-09-27 11:05:02 -0400

    Update streamlit_app.py
    error

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 # lets put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
