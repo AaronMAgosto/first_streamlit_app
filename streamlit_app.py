83f6479fc919aaee100d2b2ee0b593d4c03e8b26
Aaron Agosto <128488135+AaronMAgosto@users.noreply.github.com>
2023-09-27 10:46:39 -0400

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Diner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale, sinach, & rocket smoothie')
+streamlit.text('hard-boilded free range egg')
